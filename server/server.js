b7ae90e0625727a8df71b2c888f962245f425119
yogigl <erka.j.p@gmail.com>
2017-07-12 19:52:04 +0700

    validator, type, default

--- server/server.js
+++ server/server.js
@@ -3,37 +3,65 @@ var mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost:27017/TodoAppMongoos', {useMongoClient: true})
 
-// save new somthing
+
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 })
 
-var newTodo = new Todo({
-  text: 'Cook dinner'
+// var newTodo = new Todo({
+//   text: 'Cook dinner'
+// })
+//
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+// })
+
+// var otherTodo = new Todo({
+//   text: '  edit video2  ',
+//   // completed: false,
+//   // completedAt: 123
+// })
+//
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2))
+// }, (e) => {
+//   console.log('Unable to save todo', e);
+// })
+
+// user model
+// email - require  it - trim it -type -minlength 1
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, '{VALUE} is not email, Please fill a valid email address']
+  }
 })
 
-newTodo.save().then((doc) => {
-  console.log('Saved todo', doc);
-}, (e) => {
-  console.log('Unable to save todo');
+var user = new User({
+  email: 'as@as'
 })
 
-var otherTodo = new Todo({
-  text: 'Cook lunch',
-  completed: false,
-  completedAt: 123
-})
-
-otherTodo.save().then((doc) => {
+user.save().then((doc) => {
   console.log(JSON.stringify(doc, undefined, 2))
 }, (e) => {
-  console.log('Unable to save todo', e);
+  console.log('Unable to save user', e);
 })
