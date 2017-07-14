f8f5d33bfca45587ba868f3fc70abdfc5d0f056f
yogigl <erka.j.p@gmail.com>
2017-07-14 19:53:48 +0700

    set env production dan logic mongodb connect flexible

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,15 @@
 var mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/TodoAppMongoos'||'mongodb://todo-udemy:59687d457fb88642b35459b0@ds157682.mlab.com:57682/todo-api-udemy', {useMongoClient: true})
+var mongodbConnection = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return 'mongodb://todo-udemy:59687d457fb88642b35459b0@ds157682.mlab.com:57682/todo-api-udemy'
+  }
+  return 'mongodb://localhost:27017/TodoAppMongoos'
+}
+
+// mongoose.connect('mongodb://localhost:27017/TodoAppMongoos', {useMongoClient: true})
+mongoose.connect(mongodbConnection(), {useMongoClient: true})
 // 59687d457fb88642b35459b0
 // mongodb://todo-udemy:59687d457fb88642b35459b0@ds157682.mlab.com:57682/todo-api-udemy
 module.export = {mongoose}
