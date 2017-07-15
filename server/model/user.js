32cb53ea06bc44df40610d67ddcc1c3e30fcd5ad
yogigl <erka.j.p@gmail.com>
2017-07-16 00:11:34 +0700

    add post /users

--- server/model/user.js
+++ server/model/user.js
@@ -1,4 +1,14 @@
-var mongoose = require('mongoose')
+const mongoose = require('mongoose')
+const validator = require('validator');
+
+// {
+//   email: 'a@a.com',
+//   password: 'myPassword',
+//   tokens: {
+//     acces: 'auth',
+//     token: 'sasdasdqwe241321as'
+//   }
+// }
 
 var User = mongoose.model('User', {
   email: {
@@ -6,8 +16,29 @@ var User = mongoose.model('User', {
     required: true,
     trim: true,
     minlength: 1,
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, '{VALUE} is not email, Please fill a valid email address']
-  }
+    // match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, '{VALUE} is not email, Please fill a valid email address'],
+    unique: true,
+    validate: {
+      isAsync: false,
+      validator: validator.isEmail,
+      message: '{VALUE} is not valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
 
 module.exports = {User}
