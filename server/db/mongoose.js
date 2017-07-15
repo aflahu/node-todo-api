997ea3b13128de21eb0d2a8893b13a63ad84181b
yogigl <erka.j.p@gmail.com>
2017-07-15 19:43:05 +0700

    set up seperate test and development env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -9,7 +9,7 @@ mongoose.Promise = global.Promise
 // }
 
 // mongoose.connect('mongodb://localhost:27017/TodoAppMongoos', {useMongoClient: true})
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoAppMongoos', {useMongoClient: true})
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true})
 // 59687d457fb88642b35459b0
 // mongodb://todo-udemy:59687d457fb88642b35459b0@ds157682.mlab.com:57682/todo-api-udemy
 module.export = {mongoose}
