b0c630c3da6524a64b521c2c0daf00114d546c77
yogigl <erka.j.p@gmail.com>
2017-07-16 19:26:35 +0700

    generate auth token and setting headers

--- server/model/user.js
+++ server/model/user.js
@@ -1,16 +1,9 @@
 const mongoose = require('mongoose')
-const validator = require('validator');
+const validator = require('validator')
+const jwt = require('jsonwebtoken');
+const _ = require('lodash')
 
-// {
-//   email: 'a@a.com',
-//   password: 'myPassword',
-//   tokens: {
-//     acces: 'auth',
-//     token: 'sasdasdqwe241321as'
-//   }
-// }
-
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -40,5 +33,26 @@ var User = mongoose.model('User', {
     }
   }]
 })
+
+UserSchema.methods.toJSON = function () {
+  var user = this
+  var userObject = user.toObject()
+
+  return _.pick(userObject, ['_id', 'email'])
+}
+
+UserSchema.methods.generateAuthToken = function() {
+  var user = this
+  var access = 'auth'
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc12').toString()
+
+  user.tokens.push({access, token})
+
+  return user.save().then(() => {
+    return token
+  })
+}
+
+var User = mongoose.model('User', UserSchema)
 
 module.exports = {User}
