eb59fb4c7ad44b1db21efa6de9fc01a67e2f8438
yogigl <erka.j.p@gmail.com>
2017-07-18 00:14:23 +0700

    add POST /users/login

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express')
 const bodyParser = require('body-parser')
 const {ObjectID} = require('mongodb')
+const bcrypt = require('bcryptjs');
 
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./model/todo')
@@ -88,8 +89,8 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send()
   })
-
 })
+
 
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password'])
@@ -107,6 +108,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user)
+})
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password'])
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user)
+    })
+  }).catch((e)=> {
+    res.status(400).send()
+  })
+
 })
 
 app.listen(port, () => {
