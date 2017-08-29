b136d74c672302a0c58d7c34205d3c52cf6cf4c8
yogigl <erka.j.p@gmail.com>
2017-08-29 09:59:24 +0700

    change jshint to eslint

--- server/model/user.js
+++ server/model/user.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
+const mongoose = require('mongoose');
+const validator = require('validator');
 const jwt = require('jsonwebtoken');
-const _ = require('lodash')
+const _ = require('lodash');
 const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
@@ -10,55 +10,62 @@ var UserSchema = new mongoose.Schema({
     required: true,
     trim: true,
     minlength: 1,
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, '{VALUE} is not email, Please fill a valid email address'],
-    unique: true
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      '{VALUE} is not email, Please fill a valid email address',
+    ],
+    unique: true,
   },
   password: {
     type: String,
     require: true,
-    minlength: 6
+    minlength: 6,
   },
-  tokens: [{
-    access: {
-      type: String,
-      required: true
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true,
+      },
+      token: {
+        type: String,
+        required: true,
+      },
     },
-    token: {
-      type: String,
-      required: true
-    }
-  }]
-})
+  ],
+});
 
-UserSchema.methods.toJSON = function () {
-  var user = this
-  var userObject = user.toObject()
+UserSchema.methods.toJSON = function() {
+  var user = this;
+  var userObject = user.toObject();
 
-  return _.pick(userObject, ['_id', 'email'])
-}
+  return _.pick(userObject, ['_id', 'email']);
+};
 
 UserSchema.methods.generateAuthToken = function() {
-  var user = this
-  var access = 'auth'
-  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString()
+  var user = this;
+  var access = 'auth';
+  var token = jwt
+    .sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET)
+    .toString();
 
-  user.tokens.push({access, token})
+  user.tokens.push({ access, token });
 
   return user.save().then(() => {
-    return token
-  })
-}
+    return token;
+  });
+};
 
-UserSchema.methods.removeToken = function (token) {
-  var user = this
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
   return user.update({
     $pull: {
-      tokens: {token}
-    }
-  })
-}
+      tokens: { token },
+    },
+  });
+};
 
-UserSchema.statics.findByToken = function (token) {
+UserSchema.statics.findByToken = function(token) {
   var User = this;
   var decoded;
 
@@ -69,48 +76,46 @@ UserSchema.statics.findByToken = function (token) {
   }
 
   return User.findOne({
-    '_id': decoded._id,
+    _id: decoded._id,
     'tokens.token': token,
-    'tokens.access': 'auth'
+    'tokens.access': 'auth',
   });
 };
 
-UserSchema.statics.findByCredentials = function (email, password) {
-  var User = this
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
 
-  return User.findOne({email}).then((user) => {
+  return User.findOne({ email }).then(user => {
     if (!user) {
-        return Promise.reject()
+      return Promise.reject();
     }
     return new Promise((resolve, reject) => {
       bcrypt.compare(password, user.password, (err, res) => {
         if (res) {
-          resolve(user)
+          resolve(user);
         } else {
-          reject()
+          reject();
         }
-      })
+      });
+    });
+  });
+};
 
-    })
-  })
-}
-
-UserSchema.pre('save', function (next) {
-  var user = this
+UserSchema.pre('save', function(next) {
+  var user = this;
 
   if (user.isModified('password')) {
-     bcrypt.genSalt(10, (err, salt) => {
-       bcrypt.hash(user.password, salt, (err, hash) => {
-        user.password = hash
-        next()
-      })
-     })
-
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
   } else {
-    next()
+    next();
   }
-})
+});
 
-var User = mongoose.model('User', UserSchema)
+var User = mongoose.model('User', UserSchema);
 
-module.exports = {User}
+module.exports = { User };
